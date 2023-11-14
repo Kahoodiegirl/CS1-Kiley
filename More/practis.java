b21a23f20b0fde30e262ec04e7123f909f9a1499
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-11-14 23:28:47 +0000

    hi

---
+++ More/practis.java
@@ -0,0 +1,15 @@
+{
+    Public static void main (String args[])
+    {
+        system.out.print ("1");
+        {
+             system.out.print ("2 3");
+             {
+                 system.out.print ("4 5 6");
+                 {
+                     system.out.print ("7 8 9 10");
+                 }
+             }
+        }
+    }
+}

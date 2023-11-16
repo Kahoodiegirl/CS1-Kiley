2db1a202c04eca3f24c8656fc1b3bda1f6197453
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-11-16 15:45:47 +0000

    yessssss

---
+++ classmini/Problems.java
@@ -0,0 +1,15 @@
+class Miniproblems 
+{
+    Public static void main (String args[])
+
+        system.out.print ("*");
+        
+        system.out.print ("**");
+        
+        system.out.print ("***");
+    
+        system.out.print ("****");
+        
+        system.out.print ("***** */");
+    
+}
---
+++ classmini/problem2.java
@@ -0,0 +1,14 @@
+classmini
+{
+    public static void main (String args[])
+}
+
+    system.out.print ("*    *");
+        
+    system.out.print ("*  *");
+        
+    system.out.print ("* *");
+        
+    system.out.print ("**");
+        
+    system.out.print ("*  */");

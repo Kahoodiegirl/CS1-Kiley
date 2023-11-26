e689875be39827d68bd68fdf1bcc5c36acac9d0c
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-11-26 22:57:55 +0000

    yet

---
+++ art/printingart.java
@@ -0,0 +1,19 @@
+art
+{
+    public static void main (String args[])
+    {
+        system.out.print ("Hello");
+    }
+    system.out.print ("*");
+    system.out.print ("**");
+    system.out.print ("* *");
+    system.out.print ("* *");
+    system.out.print ("*  *");
+    system.out.print ("* *");
+    system.out.print ("**");
+    system.out.print ("*");
+
+    {
+        system.out.print ("goodbye");
+    }
+}

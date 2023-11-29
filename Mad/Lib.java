42f7beec317226ac9471321cb2d4af25f9a2ecb7
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-11-29 13:18:06 +0000

    yess

---
+++ Mad/Lib.java
@@ -0,0 +1,31 @@
+class Lib
+{
+    public static void main(String args[])
+    {
+        //Define and Initalize 10 variables that coorespond with the blanks in your program
+
+        String noun1 = "Carrot";
+        String verb1 = "Running";
+        Staring adjective1 = "fluffy"
+        String verb2 = "crying"
+        String noun2 = "ramen noodles"
+        String common noun1 = "food"
+        String verb3 = "screaming"
+        String verb4 = "buying"
+        String common noun2 = "cat"
+        String adjective2 = "crazy"
+    
+        //Write story with concatenated values
+        System.out.print("once upon a time there was a " + noun1);
+        System.out.print("and it was in the field " + verb1);
+        System.out.print("There was a monster that was " + adjective1);
+        System.out.print("and the monster is " + verb2);
+        System.out.print("The carrot depressingly eatting " + noun2);
+        System.out.print("The fluffy moster cries more because it wanted " + common noun1);
+        System.out.print("The carrot was " + verb3);
+        System.out.print("and the monster was " + verb4);
+        System.out.print("a " + common noun2);
+        System.out.print("The carrot and monster are " + adjective2);
+        System.out.print("Thats the end of the story");
+    }
+}

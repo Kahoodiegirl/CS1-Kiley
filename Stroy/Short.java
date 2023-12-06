35dfb310a52543b78179d2c2fcedcdb2a4e7d416
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-06 14:51:46 +0000

    hi

---
+++ Stroy/Short.java
@@ -0,0 +1,11 @@
+{
+    Public static void main (String args[])
+    {
+        system.out.print ("The creppy forist");
+        System.out.print ("on day on hallowen");
+        System.out.print ("the main character is Carl: Where are we?");
+        System.out.print ("other character Kate: we are in the forist");
+        System.out.print ("They walk and they found a rock and it moves and they run away and go to there houses");
+        System.out.print ("The end");
+    }
+}

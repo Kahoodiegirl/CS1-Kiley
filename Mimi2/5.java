fc9074e009b413e4d62727e79c00983eea836225
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 17:20:05 +0000

    yes

---
+++ Mimi2/5.java
@@ -0,0 +1,17 @@
+class 5
+{
+    public static void main(String args[])
+    {
+        System.out.print("Enter price: ");
+        int price=sc,nextInt();
+        System.out.print("Enter quntity: ");
+        int quantity=sc.nextInt();
+        int discout= (total_cost/100)*10;
+        int net_amout=total_cost-discount;
+
+        System.out.println("Price of one Item= "+price);
+        System.out.println("Proce of "+total_cost+"="+discount);
+        System.out.println("10% discount on "+total_cost+"="+discount);
+        System.out,pringln("Net amount to be paid=" +toutal_cost+"-"+discount+"="+net_amount);
+    }
+}

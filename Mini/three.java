7c88b98aa5fee3a06a2bfb9e919286c258e3ad50
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 15:18:21 +0000

    he

---
+++ Mini/three.java
@@ -0,0 +1,15 @@
+class three 
+{
+    public static void main(String args[])
+    {
+       #include<stdio.h>
+       int main()
+       float length, width, area;
+       printf("Enter length of rectangle\n");
+       scanf("%f", &with);
+       
+       area = length * width;
+       printf("Area of Rectangle is")
+       return 0;
+    }
+}

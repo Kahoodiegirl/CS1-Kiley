146319195a132e7f8311992a98d7d4ec20eb7b24
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 22:50:33 +0000

    yessssssssssss

---
+++ Mimi4/7.java
@@ -0,0 +1,12 @@
+class 7
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=100;i++){
+            printf("%d^2 =\t%d"i,i*i);
+            printf("\n");
+        }
+        return 0;
+    }
+}

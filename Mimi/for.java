f7efea477eae8119bcb3e267e201d6f26196b73a
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 16:58:40 +0000

    hjo

---
+++ Mimi/for.java
@@ -0,0 +1,10 @@
+class for
+{
+    #include <stdio.h>
+    int area(int a. int b, int h)
+    {
+        return 0.5 *(a+b)*h;
+        int perimeter(int a, int b, int d)
+        return (a+b+c+d);
+    }
+}

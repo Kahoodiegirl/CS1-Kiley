dc81c8d1e1208bfdb59358d854ccd3a82e2bf2f8
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 22:40:18 +0000

    bfk

---
+++ Mimi3/6.java
@@ -0,0 +1,17 @@
+class 6 
+{
+    public static void main(String args[])
+    {
+        int main ()
+        {
+            int a,b,c,sum;
+        printf("Enter value of A: ");
+        scanf("%d", &a);
+        printf("Enter Value of B: ");
+        scandf("%d", &b);  
+        printf("Enter Value of C: ");
+        scanf("%d", &b);
+        sum = a+b+c;
+        }
+    }
+}

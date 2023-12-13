5c928d1d76f8c2515eeb4777fc2fa31c64a27222
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 23:08:34 +0000

    h

---
+++ Mimi5/8.java
@@ -0,0 +1,15 @@
+class 7
+{
+    public static void main (String args[])
+    {
+        int number, cube;
+        printf("\n Please Enter any inter Value : ");
+        scanf("%d, &number");
+
+        cube = number * number * number;
+
+        printf("\n Cube of a given %d is = %d. number, cube");
+
+        return 0;
+    }
+}

4efbea09a339c7a47f1005e3753baf42135cfe24
Kahoodiegirl <149598433+Kahoodiegirl@users.noreply.github.com>
2023-12-13 23:17:29 +0000

    jg

---
+++ Mimi6/9.java
@@ -0,0 +1,11 @@
+class 9
+{
+    public static void main(String args[])
+    {
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Enter 1 to convert from Celcius to Fahrenheit.");
+        System.out.println("ENter 2 to convert fro, Fahreheit to Celcius.");
+        int choice = scan.nextInt();
+    }
+}
